1e9768e0be8bd299df249b6a3123309a7d8138e4
ballerin <francesco@ballerin.it>
2019-03-19 23:46:13 +0100

    Started writing mega switch

--- TobiChallenge/chat/utils.py
+++ TobiChallenge/chat/utils.py
@@ -7,24 +7,41 @@ def luis_connector(query):
     print(json_res)
 
     # Perform insertion of chat attributes (intent and entities)
-    list_of_entities = []
+    dict_of_entities = {}
     for el in json_res['entities']:
-        list_of_entities.append({'value': el['resolution']['values'][0],
-                             'type': el['type']
-                             })
-    print(list_of_entities)
+        dict_of_entities[el['type']] =  el['resolution']['values']
+    print(dict_of_entities)
     message = Message(
         text=json_res['query'],
         top_scoring_intent=json_res['topScoringIntent']['intent'],
         score_top_intent=json_res['topScoringIntent']['score'],
         sentiment_analysis=json_res['sentimentAnalysis']['score'],
-        entities=list_of_entities
+        entities=dict_of_entities
     )
     message.save()
 
-    print(message)
+    return message
 
+
+def create_reply(message):
+    """
+    Basically a huge IF wall
+    :param message:
+    :return:
+    """
+    top = message.top_scoring_intent
+    reply = ""
+    if top == "CallSupport":
+        reply = "Stanno tutti a dormire"
+    elif top == "CancelFromExam":
+        en = message.entities
+        if len(en['Exams'])>1:
+            reply = "Quale esame?\n"
+        else:
+            reply = "OK"
+    return reply
 
 def manage_chat(text_of_message, user):
-    luis_connector(text_of_message)
-    return str(text_of_message) + " " + str(user)
+    message = luis_connector(text_of_message)
+    reply = create_reply(message)
+    return reply
